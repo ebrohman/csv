829b30a575fa2cc03346d012372712c0640fb073
ebrohman <ericbrohman@gmail.com>
2014-10-26 15:32:39 -0500

    put into an array

--- csv_example.rb
+++ csv_example.rb
@@ -2,8 +2,10 @@ require 'CSV'
 
 csv_text = File.read("numbers.csv")
 csv = CSV.parse(csv_text)
+@rows = []
 csv.each do |row|
-  p row.each {|x| x.sub! /\A0+/, ''}
+   @rows << row.each {|x| x.sub! /\A0+/, ''}
 end
 
+p @rows
 

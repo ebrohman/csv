3c0d55c28eeddd35394f7715d5006af0e9b48eca
ebrohman <ericbrohman@gmail.com>
2014-10-27 08:39:09 -0500

    remove variable

--- csv_example.rb
+++ csv_example.rb
@@ -3,8 +3,7 @@ require 'CSV'
 CSV.open("new_numbers.csv", "w", :headers => true) do |csv|
   CSV.foreach("numbers.csv", headers: :true, converters: :numeric) do |obj|
     csv << obj.headers.sort
-    values = Hash[obj.sort].values
-    csv << values
+    csv << Hash[obj.sort].values
   end
 end
 
